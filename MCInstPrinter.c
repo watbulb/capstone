5ccc5414aa3ee143e82668a6033dfa67890bf983
Rot127 <unisono@quyllur.org>
2023-02-26 13:24:53 -0500

    Format MCInstPrinter.*

--- MCInstPrinter.c
+++ MCInstPrinter.c
@@ -7,169 +7,153 @@
 
 extern bool ARM_getFeatureBits(unsigned int mode, unsigned int feature);
 
-static bool testFeatureBits(const MCInst *MI, uint32_t Value) {
-  assert(MI);
-  switch (MI->csh->arch) {
-  default: assert(0 && "Not implemented for current arch.");
-  case CS_ARCH_ARM:
-    return ARM_getFeatureBits(MI->csh->mode, Value);
-  }
+static bool testFeatureBits(const MCInst *MI, uint32_t Value)
+{
+	assert(MI);
+	switch (MI->csh->arch) {
+	default:
+		assert(0 && "Not implemented for current arch.");
+	case CS_ARCH_ARM:
+		return ARM_getFeatureBits(MI->csh->mode, Value);
+	}
 }
 
-static bool matchAliasCondition(MCInst *MI,
-                                const MCRegisterInfo *MRI, unsigned *OpIdx,
-                                const AliasMatchingData *M,
-                                const AliasPatternCond *C,
-                                bool *OrPredicateResult) {
-  // Feature tests are special, they don't consume operands.
-  if (C->Kind == AliasPatternCond_K_Feature)
-    return testFeatureBits(MI, C->Value);
-  if (C->Kind == AliasPatternCond_K_NegFeature)
-    return !testFeatureBits(MI, C->Value);
-  // For feature tests where just one feature is required in a list, set the
-  // predicate result bit to whether the expression will return true, and only
-  // return the real result at the end of list marker.
-  if (C->Kind == AliasPatternCond_K_OrFeature) {
-    *OrPredicateResult |=  testFeatureBits(MI, C->Value);
-    return true;
-  }
-  if (C->Kind == AliasPatternCond_K_OrNegFeature) {
-    *OrPredicateResult |= !(testFeatureBits(MI, C->Value));
-    return true;
-  }
-  if (C->Kind == AliasPatternCond_K_EndOrFeatures) {
-    bool Res = *OrPredicateResult;
-    *OrPredicateResult = false;
-    return Res;
-  }
+static bool matchAliasCondition(MCInst *MI, const MCRegisterInfo *MRI,
+								unsigned *OpIdx, const AliasMatchingData *M,
+								const AliasPatternCond *C,
+								bool *OrPredicateResult)
+{
+	// Feature tests are special, they don't consume operands.
+	if (C->Kind == AliasPatternCond_K_Feature)
+		return testFeatureBits(MI, C->Value);
+	if (C->Kind == AliasPatternCond_K_NegFeature)
+		return !testFeatureBits(MI, C->Value);
+	// For feature tests where just one feature is required in a list, set the
+	// predicate result bit to whether the expression will return true, and only
+	// return the real result at the end of list marker.
+	if (C->Kind == AliasPatternCond_K_OrFeature) {
+		*OrPredicateResult |= testFeatureBits(MI, C->Value);
+		return true;
+	}
+	if (C->Kind == AliasPatternCond_K_OrNegFeature) {
+		*OrPredicateResult |= !(testFeatureBits(MI, C->Value));
+		return true;
+	}
+	if (C->Kind == AliasPatternCond_K_EndOrFeatures) {
+		bool Res = *OrPredicateResult;
+		*OrPredicateResult = false;
+		return Res;
+	}
 
-  // Get and consume an operand.
-  MCOperand *Opnd = MCInst_getOperand(MI, *OpIdx);
-  ++OpIdx;
+	// Get and consume an operand.
+	MCOperand *Opnd = MCInst_getOperand(MI, *OpIdx);
+	++OpIdx;
 
-  // Check the specific condition for the operand.
-  switch (C->Kind) {
-  case AliasPatternCond_K_Imm:
-    // Operand must be a specific immediate.
-    return MCOperand_isImm(Opnd) && MCOperand_getImm(Opnd) == (int32_t)C->Value;
-  case AliasPatternCond_K_Reg:
-    // Operand must be a specific register.
-    return MCOperand_isReg(Opnd) && MCOperand_getReg(Opnd) == C->Value;
-  case AliasPatternCond_K_TiedReg:
-    // Operand must match the register of another operand.
-    return MCOperand_isReg(Opnd) && MCOperand_getReg(Opnd) == MCOperand_getReg(MCInst_getOperand(MI, C->Value));
-  case AliasPatternCond_K_RegClass:
-    // Operand must be a register in this class. Value is a register class id.
-    return MCOperand_isReg(Opnd) && MCRegisterClass_contains(MCRegisterInfo_getRegClass(MRI, C->Value), MCOperand_getReg(Opnd));
-  case AliasPatternCond_K_Custom:
-    // Operand must match some custom criteria.
-    // return M.ValidateMCOperand(Opnd, *STI, C->Value);
-    assert(0 && "Custom operand condition check not implemented.");
-  case AliasPatternCond_K_Ignore:
-    // Operand can be anything.
-    return true;
-  case AliasPatternCond_K_Feature:
-  case AliasPatternCond_K_NegFeature:
-  case AliasPatternCond_K_OrFeature:
-  case AliasPatternCond_K_OrNegFeature:
-  case AliasPatternCond_K_EndOrFeatures:
-    assert(0 && "handled earlier");
-  }
-  assert(0 && "invalid kind");
+	// Check the specific condition for the operand.
+	switch (C->Kind) {
+	case AliasPatternCond_K_Imm:
+		// Operand must be a specific immediate.
+		return MCOperand_isImm(Opnd) &&
+			   MCOperand_getImm(Opnd) == (int32_t)C->Value;
+	case AliasPatternCond_K_Reg:
+		// Operand must be a specific register.
+		return MCOperand_isReg(Opnd) && MCOperand_getReg(Opnd) == C->Value;
+	case AliasPatternCond_K_TiedReg:
+		// Operand must match the register of another operand.
+		return MCOperand_isReg(Opnd) &&
+			   MCOperand_getReg(Opnd) ==
+				   MCOperand_getReg(MCInst_getOperand(MI, C->Value));
+	case AliasPatternCond_K_RegClass:
+		// Operand must be a register in this class. Value is a register class
+		// id.
+		return MCOperand_isReg(Opnd) &&
+			   MCRegisterClass_contains(
+				   MCRegisterInfo_getRegClass(MRI, C->Value),
+				   MCOperand_getReg(Opnd));
+	case AliasPatternCond_K_Custom:
+		// Operand must match some custom criteria.
+		// return M.ValidateMCOperand(Opnd, *STI, C->Value);
+		assert(0 && "Custom operand condition check not implemented.");
+	case AliasPatternCond_K_Ignore:
+		// Operand can be anything.
+		return true;
+	case AliasPatternCond_K_Feature:
+	case AliasPatternCond_K_NegFeature:
+	case AliasPatternCond_K_OrFeature:
+	case AliasPatternCond_K_OrNegFeature:
+	case AliasPatternCond_K_EndOrFeatures:
+		assert(0 && "handled earlier");
+	}
+	assert(0 && "invalid kind");
 }
 
 /// Check if PatternsForOpcode is all zero.
-static inline bool validOpToPatter(const PatternsForOpcode *P) {
-  return !(P->Opcode == 0 && P->PatternStart == 0 && P->NumPatterns == 0);
+static inline bool validOpToPatter(const PatternsForOpcode *P)
+{
+	return !(P->Opcode == 0 && P->PatternStart == 0 && P->NumPatterns == 0);
 }
 
-const char *matchAliasPatterns(MCInst *MI, const AliasMatchingData *M) {
-  // TODO Rewrite to C
-  
-  // auto It = lower_bound(M.OpToPatterns, MI->getOpcode(),
-  //                       [](const PatternsForOpcode &L, unsigned Opcode) {
-  //                         return L.Opcode < Opcode;
-  //                       });
-  // if (It == M.OpToPatterns.end() || It->Opcode != MI->getOpcode())
-  //   return nullptr;
+const char *matchAliasPatterns(MCInst *MI, const AliasMatchingData *M)
+{
+	// TODO Rewrite to C
 
-  // Binary search by opcode. Return false if there are no aliases for this
-  // opcode.
-  unsigned MIOpcode = MI->Opcode;
-  size_t i = 0;
-  uint32_t PatternOpcode = M->OpToPatterns[i].Opcode;
-  while (PatternOpcode < MIOpcode && validOpToPatter(&M->OpToPatterns[i]))
-    PatternOpcode = M->OpToPatterns[++i].Opcode;
-  if (PatternOpcode != MI->Opcode || !validOpToPatter(&M->OpToPatterns[i]))
-    return NULL;
+	// auto It = lower_bound(M.OpToPatterns, MI->getOpcode(),
+	//                       [](const PatternsForOpcode &L, unsigned Opcode) {
+	//                         return L.Opcode < Opcode;
+	//                       });
+	// if (It == M.OpToPatterns.end() || It->Opcode != MI->getOpcode())
+	//   return nullptr;
 
-  // // Try all patterns for this opcode.
-  uint32_t AsmStrOffset = ~0U;
-  const AliasPattern *Patterns = M->Patterns + M->OpToPatterns->PatternStart;
-  for (const AliasPattern *P = Patterns; P != Patterns + M->OpToPatterns->NumPatterns; ++P) {
-    // Check operand count first.
-    if (MCInst_getNumOperands(MI) != P->NumOperands)
-      return NULL;
+	// Binary search by opcode. Return false if there are no aliases for this
+	// opcode.
+	unsigned MIOpcode = MI->Opcode;
+	size_t i = 0;
+	uint32_t PatternOpcode = M->OpToPatterns[i].Opcode;
+	while (PatternOpcode < MIOpcode && validOpToPatter(&M->OpToPatterns[i]))
+		PatternOpcode = M->OpToPatterns[++i].Opcode;
+	if (PatternOpcode != MI->Opcode || !validOpToPatter(&M->OpToPatterns[i]))
+		return NULL;
 
-    // Test all conditions for this pattern.
-    const AliasPatternCond *Conds =
-        M->PatternConds + P->AliasCondStart;
-    unsigned OpIdx = 0;
-    bool OrPredicateResult = false;
-    for (const AliasPatternCond *C = Conds; C != Conds + P->NumConds; ++C) {
-      if (matchAliasCondition(MI, MI->MRI, &OpIdx, M, C, &OrPredicateResult)) {
-        AsmStrOffset = P->AsmStrOffset;
-        break;
-      }
-    }
-    if (AsmStrOffset != ~0U)
-      break;
-  }
-  // If no alias matched, don't print an alias.
-  if (AsmStrOffset == ~0U)
-    return NULL;
+	// // Try all patterns for this opcode.
+	uint32_t AsmStrOffset = ~0U;
+	const AliasPattern *Patterns = M->Patterns + M->OpToPatterns->PatternStart;
+	for (const AliasPattern *P = Patterns;
+		 P != Patterns + M->OpToPatterns->NumPatterns; ++P) {
+		// Check operand count first.
+		if (MCInst_getNumOperands(MI) != P->NumOperands)
+			return NULL;
 
-  // Go to offset AsmStrOffset and use the null terminated string there. The
-  // offset should point to the beginning of an alias string, so it should
-  // either be zero or be preceded by a null byte.
-  return M->AsmStrings + AsmStrOffset;
+		// Test all conditions for this pattern.
+		const AliasPatternCond *Conds = M->PatternConds + P->AliasCondStart;
+		unsigned OpIdx = 0;
+		bool OrPredicateResult = false;
+		for (const AliasPatternCond *C = Conds; C != Conds + P->NumConds; ++C) {
+			if (matchAliasCondition(MI, MI->MRI, &OpIdx, M, C,
+									&OrPredicateResult)) {
+				AsmStrOffset = P->AsmStrOffset;
+				break;
+			}
+		}
+		if (AsmStrOffset != ~0U)
+			break;
+	}
+	// If no alias matched, don't print an alias.
+	if (AsmStrOffset == ~0U)
+		return NULL;
 
-  //   // Test all conditions for this pattern.
-  //   ArrayRef<AliasPatternCond> Conds =
-  //       M.PatternConds.slice(P.AliasCondStart, P.NumConds);
-  //   unsigned OpIdx = 0;
-  //   bool OrPredicateResult = false;
-  //   if (llvm::all_of(Conds, [&](const AliasPatternCond &C) {
-  //         return matchAliasCondition(*MI, STI, MRI, OpIdx, M, C,
-  //                                    OrPredicateResult);
-  //       })) {
-  //     // If all conditions matched, use this asm string.
-  //     AsmStrOffset = P.AsmStrOffset;
-  //     break;
-  //   }
-  // }
-
-  // // If no alias matched, don't print an alias.
-  // if (AsmStrOffset == ~0U)
-  //   return nullptr;
-
-  // // Go to offset AsmStrOffset and use the null terminated string there. The
-  // // offset should point to the beginning of an alias string, so it should
-  // // either be zero or be preceded by a null byte.
-  // assert(AsmStrOffset < M.AsmStrings.size() &&
-  //        (AsmStrOffset == 0 || M.AsmStrings[AsmStrOffset - 1] == '\0') &&
-  //        "bad asm string offset");
-  // return M.AsmStrings.data() + AsmStrOffset;
-  return "Alias not implemented.";
+	// Go to offset AsmStrOffset and use the null terminated string there. The
+	// offset should point to the beginning of an alias string, so it should
+	// either be zero or be preceded by a null byte.
+	return M->AsmStrings + AsmStrOffset;
 }
-
 // TODO Add functionality to toggle the flag.
 bool getUseMarkup() { return false; }
 
 /// Utility functions to make adding mark ups simpler.
-const char *markup(const char *s) {
-  if (getUseMarkup())
-    return s;
-  else
-    return "";
+const char *markup(const char *s)
+{
+	if (getUseMarkup())
+		return s;
+	else
+		return "";
 }
--- MCInstPrinter.h
+++ MCInstPrinter.h
@@ -10,53 +10,53 @@
 
 /// Returned by getMnemonic() of the AsmPrinters.
 typedef struct {
-  const char *first; // Menmonic
-  uint64_t second;   // Bits
+	const char *first; // Menmonic
+	uint64_t second;	 // Bits
 } MnemonicBitsInfo;
 
 /// Map from opcode to pattern list by binary search.
 typedef struct {
-  uint32_t Opcode;
-  uint16_t PatternStart;
-  uint16_t NumPatterns;
+	uint32_t Opcode;
+	uint16_t PatternStart;
+	uint16_t NumPatterns;
 } PatternsForOpcode;
 
 /// Data for each alias pattern. Includes feature bits, string, number of
 /// operands, and a variadic list of conditions to check.
 typedef struct {
-  uint32_t AsmStrOffset;
-  uint32_t AliasCondStart;
-  uint8_t NumOperands;
-  uint8_t NumConds;
+	uint32_t AsmStrOffset;
+	uint32_t AliasCondStart;
+	uint8_t NumOperands;
+	uint8_t NumConds;
 } AliasPattern;
 
 typedef enum {
-  AliasPatternCond_K_Feature,	    // Match only if a feature is enabled.
-  AliasPatternCond_K_NegFeature,    // Match only if a feature is disabled.
-  AliasPatternCond_K_OrFeature,	    // Match only if one of a set of features is
-				    // enabled.
-  AliasPatternCond_K_OrNegFeature,  // Match only if one of a set of features is
-				    // disabled.
-  AliasPatternCond_K_EndOrFeatures, // Note end of list of K_Or(Neg)?Features.
-  AliasPatternCond_K_Ignore,	    // Match any operand.
-  AliasPatternCond_K_Reg,	    // Match a specific register.
-  AliasPatternCond_K_TiedReg,	    // Match another already matched register.
-  AliasPatternCond_K_Imm,	    // Match a specific immediate.
-  AliasPatternCond_K_RegClass,	    // Match registers in a class.
-  AliasPatternCond_K_Custom,	    // Call custom matcher by index.
+	AliasPatternCond_K_Feature,				// Match only if a feature is enabled.
+	AliasPatternCond_K_NegFeature,		// Match only if a feature is disabled.
+	AliasPatternCond_K_OrFeature,			// Match only if one of a set of features is
+																		// enabled.
+	AliasPatternCond_K_OrNegFeature,	// Match only if one of a set of features is
+																		// disabled.
+	AliasPatternCond_K_EndOrFeatures, // Note end of list of K_Or(Neg)?Features.
+	AliasPatternCond_K_Ignore,				// Match any operand.
+	AliasPatternCond_K_Reg,						// Match a specific register.
+	AliasPatternCond_K_TiedReg,				// Match another already matched register.
+	AliasPatternCond_K_Imm,						// Match a specific immediate.
+	AliasPatternCond_K_RegClass,			// Match registers in a class.
+	AliasPatternCond_K_Custom,				// Call custom matcher by index.
 } AliasPatternCond_CondKind;
 
 typedef struct {
-  AliasPatternCond_CondKind Kind;
-  uint32_t Value;
+	AliasPatternCond_CondKind Kind;
+	uint32_t Value;
 } AliasPatternCond;
 
 /// Tablegenerated data structures needed to match alias patterns.
 typedef struct {
-  const PatternsForOpcode *OpToPatterns;
-  const AliasPattern *Patterns;
-  const AliasPatternCond *PatternConds;
-  const char *AsmStrings;
+	const PatternsForOpcode *OpToPatterns;
+	const AliasPattern *Patterns;
+	const AliasPatternCond *PatternConds;
+	const char *AsmStrings;
 } AliasMatchingData;
 
 const char *matchAliasPatterns(MCInst *MI, const AliasMatchingData *M);
